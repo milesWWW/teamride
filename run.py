2b46a5327d73cdc7b9314bef9a768894b04845c4
milesWWW <131757491+milesWWW@users.noreply.github.com>
2025-03-14 17:30:40 +0800

    Initialize logging system

--- run.py
+++ run.py
@@ -1,8 +1,9 @@
 # 创建应用实例
 import sys
-
 from wxcloudrun import app
+from wxcloudrun.logging_config import logger
 
 # 启动Flask Web服务
 if __name__ == '__main__':
+    logger.info('Starting Flask application')
     app.run(host=sys.argv[1], port=sys.argv[2])
